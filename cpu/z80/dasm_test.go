b2f3daab42ec26cb296493df2b3b257496941655
Mike McGann <mike.mcgann@blackchip.org>
2018-09-23 12:38:36 -0400

    remove invalid entries and test separately

--- cpu/z80/dasm_test.go
+++ cpu/z80/dasm_test.go
@@ -1,6 +1,7 @@
 package z80
 
 import (
+	"fmt"
 	"testing"
 
 	. "github.com/blackchip-org/pac8/expect"
@@ -21,6 +22,38 @@ func TestDasm(t *testing.T) {
 			c.Pos = 0x10
 			s := dasm.Next()
 			With(t).Expect(s.Op).ToBe(test.op)
+		})
+	}
+}
+
+func TestInvalidDD(t *testing.T) {
+	for i := 0; i <= 0xff; i++ {
+		name := fmt.Sprintf("%02x", i)
+		t.Run(name, func(t *testing.T) {
+			mem := memory.NewRAM(0x20)
+			mem.Store(0, 0xdd)
+			mem.Store(1, uint8(i))
+			dasm := cpu.NewDisassembler(mem, ReaderZ80)
+			s := dasm.Next()
+			if s.Op[0] == '?' && i != 0xdd && i != 0xed && i != 0xfd {
+				With(t).Expect(s.Op).ToBe(fmt.Sprintf("?dd%02x", i))
+			}
+		})
+	}
+}
+
+func TestInvalidFD(t *testing.T) {
+	for i := 0; i <= 0xff; i++ {
+		name := fmt.Sprintf("%02x", i)
+		t.Run(name, func(t *testing.T) {
+			mem := memory.NewRAM(0x20)
+			mem.Store(0, 0xfd)
+			mem.Store(1, uint8(i))
+			dasm := cpu.NewDisassembler(mem, ReaderZ80)
+			s := dasm.Next()
+			if s.Op[0] == '?' && i != 0xdd && i != 0xed && i != 0xfd {
+				With(t).Expect(s.Op).ToBe(fmt.Sprintf("?fd%02x", i))
+			}
 		})
 	}
 }
